c563fb734c9b21950a67ea117c1f88ea9910a5ec
1031Tomoyuki <unno-tomoyuki1031@g.ecc.u-tokyo.ac.jp>
2025-11-13 01:48:33 +0900

    refactored guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,38 +1,54 @@
-use std::io;
 use rand::Rng;
+use std::io;
 
-fn lt_eq_gt <T: PartialOrd>(input: T, ans: T) -> i32{
-    // 答えと同じなら0, 答えの方が大きければ1, 答えの方が小さければ2
-    if input == ans {
-        println!("Correct!");
-        0
-    }else if input < ans{
-        println!("Correct Answer is larger than your guess");
-        1
-    }else{
-        println!("Correct Answer is smaller than your guess");
-        2
+enum Compare {
+    Correct,
+    Smaller,
+    Larger,
+}
+
+fn read_input() -> Option<i32> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok()?;
+    let n: i32 = input.trim().parse().ok()?;
+    if (1..=100).contains(&n) {
+        Some(n)
+    } else {
+        None
     }
 }
+
+fn compare_guess(input: i32, answer: i32) -> Compare {
+    use Compare::*;
+    if input == answer {
+        Correct
+    } else if input < answer {
+        Smaller
+    } else {
+        Larger
+    }
+}
+
 
 fn main() {
     let mut rng = rand::thread_rng();
     let target = rng.gen_range(1..=100);
-    println!("1~100の整数を入力してください");
-    loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("input error");
+    let mut t = 0;
 
-        let num = match input.trim().parse() {
-            Ok(n) if (1..=100).contains(&n) => n,
-            _ => {
-                eprintln!("1~100の整数を入力してください");
-                continue;
-            }
-        };
-        if lt_eq_gt(num, target)== 0 {
-            break;
+    loop{
+        t += 1;
+        println!("{}回目の予想(1~100の間の数字を入力してください)", t);
+        
+        match read_input(){
+            Some(num) => match compare_guess(num, target){
+                Compare::Correct => {
+                    println!("{}回目で成功!", t);
+                    break;
+                }
+                Compare::Smaller => println!("もっと大きいです"),
+                Compare::Larger => println!("もっと小さいです")
+            },
+            None => println!("入力が無効です。1~100の数字を入力してください")
         }
     }
-
 }
